9f1c6ae0765a166022f2a4ea44eb601d658cf6b3
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-04-01 18:41:11 -0500

    got table working

--- customer.js
+++ customer.js
@@ -1,5 +1,7 @@
 const inquirer = require("inquirer");
 var mysql = require("mysql");
+const table = require("table");
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -9,20 +11,32 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-  console.log(err);
-  console.log("connected");
+  // console.log("connected");
   displayProducts();
 
   connection.end();
 });
+
 function displayProducts() {
   connection.query(`SELECT * FROM products`, function(err, res) {
-    console.log(err);
-    res.forEach(v => console.log(v.price));
-    console.log(res);
+    let displayTable = [];
+    res.forEach(v => displayTable.push(renderRow(v)));
+    renderTable(displayTable);
   });
+}
+
+function renderRow(v) {
+  let row = [];
+  for (var prop in v) {
+    row.push(v[prop]);
+  }
+  return row;
 }
 
 function invite() {
   //   inquirer.prompt([]);
+}
+function renderTable(data) {
+  let output = table.table(data);
+  console.log(output);
 }
